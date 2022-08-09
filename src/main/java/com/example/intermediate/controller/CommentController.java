f081b514cc71620b8d943287572803635432279c
keembogeul <318qhtmf@naver.com>
2022-08-09 22:15:04 +0900

    CommentResponseDto에 SubCommentResponseDto 추가해서 comment 하나 조회 시 대댓글 출력

--- src/main/java/com/example/intermediate/controller/CommentController.java
+++ src/main/java/com/example/intermediate/controller/CommentController.java
@@ -1,9 +1,10 @@
 package com.example.intermediate.controller;
 
-import com.example.intermediate.dto.response.ResponseDto;
-import com.example.intermediate.dto.request.CommentRequestDto;
-import com.example.intermediate.service.CommentService;
 import javax.servlet.http.HttpServletRequest;
+
+import com.example.intermediate.dto.request.CommentRequestDto;
+import com.example.intermediate.dto.response.ResponseDto;
+import com.example.intermediate.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,28 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CommentController {
 
-  private final CommentService commentService;
+    private final CommentService commentService;
 
-  @RequestMapping(value = "/api/auth/comment", method = RequestMethod.POST)
-  public ResponseDto<?> createComment(@RequestBody CommentRequestDto requestDto,
-      HttpServletRequest request) {
-    return commentService.createComment(requestDto, request);
-  }
+    @RequestMapping(value = "/api/auth/comment", method = RequestMethod.POST)
+    public ResponseDto<?> createComment(@RequestBody CommentRequestDto requestDto,
+                                        HttpServletRequest request) {
+        return commentService.createComment(requestDto, request);
+    }
 
-  @RequestMapping(value = "/api/comment/{id}", method = RequestMethod.GET)
-  public ResponseDto<?> getAllComments(@PathVariable Long id) {
-    return commentService.getAllCommentsByPost(id);
-  }
+    @RequestMapping(value = "/api/comment/detail/{id}", method = RequestMethod.GET)
+    public ResponseDto<?> getComment(@PathVariable Long id) {
+        return commentService.getComment(id);
+    }
 
-  @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.PUT)
-  public ResponseDto<?> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto,
-      HttpServletRequest request) {
-    return commentService.updateComment(id, requestDto, request);
-  }
+    @RequestMapping(value = "/api/comment/{id}", method = RequestMethod.GET)
+    public ResponseDto<?> getAllComments(@PathVariable Long id) {
+        return commentService.getAllCommentsByPost(id);
+    }
 
-  @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.DELETE)
-  public ResponseDto<?> deleteComment(@PathVariable Long id,
-      HttpServletRequest request) {
-    return commentService.deleteComment(id, request);
-  }
+    @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.PUT)
+    public ResponseDto<?> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto,
+                                        HttpServletRequest request) {
+        return commentService.updateComment(id, requestDto, request);
+    }
+
+    @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.DELETE)
+    public ResponseDto<?> deleteComment(@PathVariable Long id,
+                                        HttpServletRequest request) {
+        return commentService.deleteComment(id, request);
+    }
 }
